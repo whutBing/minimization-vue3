3229782708d581ece90ac81fc6031cde92c97706
bing <zhengyabing2012@1163.com>
2023-06-24 18:12:59 +0800

    02-初步完成effect及reactive及自动化测试

---
+++ src/reactivity/tests/effect.spec.ts
@@ -0,0 +1,38 @@
+import { effect } from "../effect";
+import { reactive } from "../reactive";
+
+//是一个测试套件，它包含了多个测试用例,也称为测试块
+describe("effect", () => {
+  // 是一个具体的测试用例
+  it("happy path", () => {
+    const user = reactive({
+      age: 10,
+    });
+
+    let nextAge;
+    effect(() => {
+      nextAge = user.age + 1;
+    });
+
+    expect(nextAge).toBe(11);
+
+    // update
+    user.age++;
+    expect(nextAge).toBe(12);
+  });
+
+  it("should return runner when call effect", () => {
+    // 当调用 runner 的时候可以重新执行 effect.run
+    // runner 的返回值就是用户给的 fn 的返回值
+    let foo = 0;
+    const runner = effect(() => {
+      foo++;
+      return foo;
+    });
+
+    expect(foo).toBe(1);
+    runner();
+    expect(foo).toBe(2);
+    expect(runner()).toBe(3);
+  });
+});
