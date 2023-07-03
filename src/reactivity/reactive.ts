5bb477a394f6e69a8fb47af8958e3d9fcebc01a0
bing <zhengyabing2012@1163.com>
2023-07-03 19:35:43 +0800

    06-实现isReadonly和isReactive

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,5 +1,10 @@
 import { mutableHandlers, readonlyHandlers } from "./baseHandler";
 
+export const enum ReactiveFlags {
+  IS_REACTIVE = "__v_isReactive",
+  IS_READONLY = "__v_isReadonly",
+}
+
 export function reactive(raw) {
   return createReactiveObject(raw, mutableHandlers);
 }
@@ -10,4 +15,13 @@ export function readonly(raw) {
 
 function createReactiveObject(target, baseHandles) {
   return new Proxy(target, baseHandles);
+}
+
+export function isReactive(value) {
+  // 访问属性会触发getter
+  return !!value[ReactiveFlags.IS_REACTIVE];
+}
+
+export function isReadonly(value) {
+  return !!value[ReactiveFlags.IS_READONLY];
 }
