d18245c3a22e039df875fc2595931f90f39e2e99
bing <zhengyabing2012@1163.com>
2023-07-04 21:28:37 +0800

    08-实现readonly和reactive嵌套兑现转换

--- src/reactivity/tests/reactive.spec.ts
+++ src/reactivity/tests/reactive.spec.ts
@@ -1,4 +1,4 @@
-import { reactive, readonly } from "../reactive";
+import { isReactive, reactive, readonly } from "../reactive";
 
 describe("reactive", () => {
   it("happy path", () => {
@@ -6,6 +6,19 @@ describe("reactive", () => {
     const observed = reactive(original);
     expect(observed).not.toBe(original);
     expect(observed.foo).toBe(1);
+  });
+
+  test("nested reactives", () => {
+    const original = {
+      nested: {
+        foo: 1,
+      },
+      array: [{ bar: 2 }],
+    };
+    const observed = reactive(original);
+    expect(isReactive(observed.nested)).toBe(true);
+    expect(isReactive(observed.array)).toBe(true);
+    expect(isReactive(observed.array[0])).toBe(true);
   });
 });
 
@@ -27,4 +40,5 @@ describe("readonly", () => {
     user.age = 11;
     expect(console.warn).toHaveBeenCalled();
   });
+
 });
--- src/reactivity/tests/readonly.spec.ts
+++ src/reactivity/tests/readonly.spec.ts
@@ -1,11 +1,25 @@
-import { isReactive, reactive } from "../reactive";
-describe("reactive", () => {
-  it("happy path", () => {
-    const original = { foo: 1 };
-    const observed = reactive(original);
-    expect(observed).not.toBe(original);
-    expect(observed.foo).toBe(1);
-    expect(isReactive(observed)).toBe(true);
-    expect(isReactive(original)).toBe(false);
+import { isReadonly, readonly } from "../reactive";
+
+describe("readonly", () => {
+  it("should make nested values readonly", () => {
+    const original = { foo: 1, bar: { baz: 2 } };
+    const wrapped = readonly(original);
+    expect(wrapped).not.toBe(original);
+    expect(isReadonly(wrapped)).toBe(true);
+    expect(isReadonly(original)).toBe(false);
+    expect(isReadonly(wrapped.bar)).toBe(true);
+    expect(isReadonly(original.bar)).toBe(false);
+
+    expect(wrapped.foo).toBe(1);
+  });
+
+  it("should call console.warn when set", () => {
+    console.warn = jest.fn();
+    const user = readonly({
+      age: 10,
+    });
+
+    user.age = 11;
+    expect(console.warn).toHaveBeenCalled();
   });
 });
