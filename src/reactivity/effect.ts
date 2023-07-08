c555bf86af9546ca2632c4a9560bc1165affb58f
bing <zhengyabing2012@1163.com>
2023-07-08 18:12:05 +0800

    11-实现ref

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,4 +1,4 @@
-import { extend } from "./shared/index";
+import { extend } from "../shared/index";
 
 // 避免硬编码
 let activeEffect;
@@ -60,21 +60,27 @@ export function track(target, key) {
   }
 
   // 反向建立effect => dep的依赖
+  trackEffects(dep);
+}
 
+export function trackEffects(dep) {
   // 看看 dep 之前有没有添加过，添加过的话 那么就不添加了
   if (dep.has(activeEffect)) return;
   dep.add(activeEffect);
   activeEffect.deps.push(dep);
 }
 
-function isTracking() {
+export function isTracking() {
   return shouldTrack && activeEffect !== undefined;
 }
 
 export function trigger(target, key) {
   let depsMap = targetMap.get(target);
   let dep = depsMap.get(key);
+  triggerEffects(dep);
+}
 
+export function triggerEffects(dep) {
   for (const effect of dep) {
     if (effect.scheduler) {
       effect.scheduler();
--- src/reactivity/shared/index.ts
+++
@@ -1,5 +0,0 @@
-export const extend = Object.assign;
-
-export const isObject = (res) => {
-  return res !== null && typeof res === "object";
-};
