c435377b7b6aaa8acaed1484e0c1dccbaeb3bf02
bing <zhengyabing2012@1163.com>
2023-08-13 14:55:23 +0800

    36-实现代码生成插值类型

--- src/compiler-core/src/transform.ts
+++ src/compiler-core/src/transform.ts
@@ -1,39 +1,56 @@
+import { NodeTypes } from "./ast";
+import { TO_DISPLAY_STRING } from "./runtimeHelpers";
+
 export function transform(root, options = {}) {
-    const context = createTransformContext(root, options);
-    traverseNode(root, context);
-    createRootCodegen(root);
+  const context = createTransformContext(root, options);
+  traverseNode(root, context);
+  createRootCodegen(root);
+
+  root.helpers = [...context.helpers.keys()];
+}
+
+function createRootCodegen(root: any) {
+  root.codegenNode = root.children[0];
+}
+
+function createTransformContext(root: any, options: any): any {
+  const context = {
+    root,
+    nodeTransforms: options.nodeTransforms || [],
+    helpers: new Map(),
+    helper(key) {
+      context.helpers.set(key, 1);
+    },
+  };
+
+  return context;
+}
+
+function traverseNode(node: any, context) {
+  const nodeTransforms = context.nodeTransforms;
+  for (let i = 0; i < nodeTransforms.length; i++) {
+    const transform = nodeTransforms[i];
+    transform(node);
   }
-  
-  function createRootCodegen(root: any) {
-    root.codegenNode = root.children[0];
+
+  switch (node.type) {
+    case NodeTypes.INTERPOLATION:
+      context.helper(TO_DISPLAY_STRING);
+      break;
+    case NodeTypes.ROOT:
+    case NodeTypes.ELEMENT:
+      traverseChildren(node, context);
+      break;
+
+    default:
+      break;
   }
-  
-  function createTransformContext(root: any, options: any): any {
-    const context = {
-      root,
-      nodeTransforms: options.nodeTransforms || [],
-    };
-  
-    return context;
+}
+function traverseChildren(node: any, context: any) {
+  const children = node.children;
+
+  for (let i = 0; i < children.length; i++) {
+    const node = children[i];
+    traverseNode(node, context);
   }
-  
-  function traverseNode(node: any, context) {
-    const nodeTransforms = context.nodeTransforms;
-    for (let i = 0; i < nodeTransforms.length; i++) {
-      const transform = nodeTransforms[i];
-      transform(node);
-    }
-  
-    traverseChildren(node, context);
-  }
-  function traverseChildren(node: any, context: any) {
-    const children = node.children;
-  
-    if (children) {
-      for (let i = 0; i < children.length; i++) {
-        const node = children[i];
-        traverseNode(node, context);
-      }
-    }
-  }
-  
+}
