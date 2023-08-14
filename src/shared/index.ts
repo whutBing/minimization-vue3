9a56be590dab257958cd9395bc32ce0d0576b86f
bing <zhengyabing2012@1163.com>
2023-08-14 23:05:10 +0800

    37-实现代码生成三种联合类型

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,38 +1,30 @@
-export const EMPTY_OBJ = {};
-
 export const extend = Object.assign;
+
+export const EMPTY_OBJ = {};
 
 export const isObject = (value) => {
   return value !== null && typeof value === "object";
 };
 
+export const isString = (value) => typeof value === "string"
+
 export const hasChanged = (val, newValue) => {
   return !Object.is(val, newValue);
 };
 
+
 export const hasOwn = (val, key) =>
   Object.prototype.hasOwnProperty.call(val, key);
 
-/**
- * 烤肉串转驼峰
- * @param str 
- * @returns 
- */
-  export const camelize = (str: string) => {
+export const camelize = (str: string) => {
   return str.replace(/-(\w)/g, (_, c: string) => {
     return c ? c.toUpperCase() : "";
   });
 };
 
-/**
- * 首字母大写
- * @param str 
- * @returns 
- */
 const capitalize = (str: string) => {
   return str.charAt(0).toUpperCase() + str.slice(1);
 };
-
 
 export const toHandlerKey = (str: string) => {
   return str ? "on" + capitalize(str) : "";
